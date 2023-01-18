a950801beeead060e45a6ed3c837aa54d6f77ce4
Anna <anisferraz@gmail.com>
2023-01-18 20:15:28 -0300

    feat: books migration

---
+++ prisma/migrations/20230117221228_books/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "books" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "bar_code" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "publisher" TEXT NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "books_bar_code_key" ON "books"("bar_code");
